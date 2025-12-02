92027cfbf84a8e091696d48d8e0f0545b1cc840b
Tor Inge Redalen <toringe@redalen.no>
2025-12-02 19:10:07 +0000

    Rename project from HERU to Sporet.no and update related paths in configuration and scripts.

--- scripts/update_manifest.py
+++ scripts/update_manifest.py
@@ -13,14 +13,14 @@ def update_manifest():
 
     # pylint: disable=unspecified-encoding
     with open(
-        f"{os.getcwd()}/custom_components/heru/manifest.json"
+        f"{os.getcwd()}/custom_components/sporet/manifest.json"
     ) as manifestfile:
         manifest = json.load(manifestfile)
 
     manifest["version"] = version
 
     with open(
-        f"{os.getcwd()}/custom_components/heru/manifest.json", "w"
+        f"{os.getcwd()}/custom_components/sporet/manifest.json", "w"
     ) as manifestfile:
         manifestfile.write(json.dumps(manifest, indent=4, sort_keys=True))
 
